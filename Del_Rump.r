6696919f66c8dad51968ba21dee1e8a4f6a87ded
Morray <Morray>
2016-04-27 09:38:25 +0800

    first modify

---
+++ Del_Rump.r
@@ -0,0 +1,12 @@
+#冗余函数改写
+switcheroo.switch<-function(x){
+  switch(x,
+         a="alligator",
+         b="bear",
+         c="camle",
+         "moose")
+}
+
+x<-c("a","b","c")
+x.out<-lapply(x,switcheroo.switch)
+x.out
