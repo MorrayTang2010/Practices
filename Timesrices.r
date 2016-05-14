a53fe01ff285169b9edeabd6c6b0b31650aa985e
Morray <Morray>
2016-05-14 17:02:19 +0800

    2016-05-11 version

--- Timesrices.r
+++ Timesrices.r
@@ -88,3 +88,68 @@ head(coef(gdpVar$varresult$Canada))
 coefplot(gdpVar$varresult$Canada)
 #predict
 predict(gdpVar,n.ahead=5)
+
+#Example4: ARIMA 
+wape = function(pred,test)
+{ 
+  len<-length(pred) 
+  errSum<-sum(abs(pred[1:len]-test[1:len])) 
+  corSum<-sum(test[1:len])
+  result<-errSum/corSum
+  result
+}
+
+
+mae = function(pred,test)
+{ 
+  errSum<-mean(abs(pred-test))    #注意  和wape的实现相比是不是简化了很多
+  errSum
+}
+
+rmse = function(pred,test)
+{ 
+  res<- sqrt(mean((pred-test)^2) )
+  res
+}
+
+#
+library(tseries)
+library(zoo)
+library(forecast)
+air <- AirPassengers
+ts.plot(air)
+passenger<-air
+
+p<-unlist(passenger)
+pt<-ts(p,frequency=12,start=2001)
+plot(pt)
+train<-window(pt,start=2001,end=2011+11/12)
+test<-window(pt,start=2012)
+
+library(forecast)
+pred_meanf<-meanf(train,h=12)
+rmse(test,pred_meanf$mean) #226.2657
+pred_naive<-naive(train,h=12)
+rmse(pred_naive$mean,test)#102.9765
+pred_snaive<-snaive(train,h=12)
+rmse(pred_snaive$mean,test)#50.70832
+pred_rwf<-rwf(train,h=12, drift=T)
+rmse(pred_rwf$mean,test)#92.66636
+pred_ses <- ses(train,h=12,initial='simple',alpha=0.2)
+rmse(pred_ses$mean,test) #89.77035
+pred_holt<-holt(train,h=12,damped=F,initial="simple",beta=0.65)
+rmse(pred_holt$mean,test)#76.86677  without beta=0.65 it would be 84.41239
+pred_hw<-hw(train,h=12,seasonal='multiplicative')
+rmse(pred_hw$mean,test)#16.36156
+fit<-ets(train)
+accuracy(predict(fit,12),test) #24.390252    结果不对...
+pred_stlf<-stlf(train)
+rmse(pred_stlf$mean,test)#22.07215
+plot(stl(train,s.window="periodic"))  #Seasonal Decomposition of Time Series by Loess
+
+fit2<-auto.arima(train)
+accuracy(forecast(fit2,h=12),test) #23.538735
+ma = arima(train, order = c(0, 1, 3),   seasonal=list(order=c(0,1,3), period=12))
+p<-predict(ma,12)
+accuracy(p$pred,test)  #18.55567
+BT = Box.test(ma$residuals, lag=30, type = "Ljung-Box", fitdf=2)
